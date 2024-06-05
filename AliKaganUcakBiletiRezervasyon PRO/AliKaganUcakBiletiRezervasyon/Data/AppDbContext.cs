7c7e8d7e1a51dbda43c57f0f97c9198997a1b498
alikagandag <133283340+alikagandag@users.noreply.github.com>
2024-06-05 23:15:46 +0300

    Add files via upload

---
+++ AliKaganUcakBiletiRezervasyon PRO/AliKaganUcakBiletiRezervasyon/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+using AliKaganUcakBiletiRezervasyon.Models;
+
+namespace AliKaganUcakBiletiRezervasyon.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=..\\..\\Data\\UcakBiletiRezervasyonDb.db");
+        }
+
+        public DbSet<Ucak> Ucak { get; set; }
+        public DbSet<Lokasyon> Lokasyon{ get; set; }
+        public DbSet<Rezervasyon> Rezervasyon { get; set; }
+    }
+}
+
---
+++ AliKaganUcakBiletiRezervasyon PRO/AliKaganUcakBiletiRezervasyon/Models/Lokasyon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliKaganUcakBiletiRezervasyon.Models
+{
+    public class Lokasyon
+    {
+        public int id { get; set; }
+        public string Ulke { get; set; }
+        public string Sehir{ get; set; }
+        public string Havaalani { get; set; }
+        public string Tarih { get; set; }
+        public string Saat { get; set; }
+     
+    }
+}
